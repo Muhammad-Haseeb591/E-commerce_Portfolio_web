f79f4f9e941d264c495f9af4de414ff3b531394a
Muhammad Haseeb <livehaseeb822@gmail.com>
2026-07-17 23:48:18 +0500

    debug code

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -24,11 +24,11 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
-  port: 587,
-  secure: false, // STARTTLS on 587
+  port: 2525,      
+  secure: false,
   auth: {
     user: process.env.BREVO_SMTP_LOGIN,
-    pass: process.env.BREVO_SMTP_KEY, 
+    pass: process.env.BREVO_SMTP_KEY,
   },
 });
 // ==============================
